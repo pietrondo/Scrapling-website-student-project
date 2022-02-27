6f5be1b8611e5c6e1095c9c46b724f537c8f1ec5
Pietro <pietro.capriata@gmail.com>
2022-02-27 00:07:18 +0000

    news

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from selenium import webdriver
 import time
+import yagmail
+import os
+
+
 
 def get_drvier():
+  
   options = webdriver.ChromeOptions()
   options.add_argument("disable-infobars")
   options.add_argument("start-maximized")
@@ -10,18 +15,31 @@ def get_drvier():
   options.add_experimental_option("excludeSwitches", ["enable-automation"])
   options.add_argument("disable-blink-features=AutomationControlled")
   driver =webdriver.Chrome(options=options)
-  driver.get("https://automated.pythonanywhere.com")
+  driver.get("https://zse.hr/en/indeks-366/365?isin=HRZB00ICBEX6")
   return driver
 
 def clean_text(text):
-  output= text.split(": ")
+  output= text.split(" ")
   return output
 
+def send_mail(numberl):
+  sender=os.environ['SENDER']
+  receiver=os.environ['RECEIVER']
+  password = os.environ['PASSWORD']
+  yag = yagmail.SMTP(user=sender, password=password)
+  yag.send(to=receiver, contents=f'Il valore è di {numberl}' , subject=f'Il valore è di {numberl}' )
 
 def main():
   driver = get_drvier()
   time.sleep(2)
-  element = driver.find_element(by="xpath",value="/html/body/div[1]/div/h1[2]")
-  return clean_text(element.text)[1]
+  element = driver.find_element(by="xpath",value='//*[@id="app_indeks"]/section[1]/div/div/div[2]/span[2]')
+  segno = clean_text(element.text)[0][0]
+  numero=clean_text(element.text)[0][0:5]
+  if(segno=="-"):
+    if(float(numero)>=0.10):
+        send_mail(numero)
 
-print(main())
+  else:
+    print("niente da inviare")
+
+main()
